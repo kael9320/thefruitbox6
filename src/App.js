8b89709631e93c97a068383d276477f61a205936
Jorge Miranda <jrm9320@gmail.com>
2022-07-21 11:04:14 -0500

    UPDATE CHALLENGE

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import NavBar from "./components/NavBar";
 import NavFooter from "./components/NavFooter";
 import ItemListContainer from "./components/ItemListContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route, link} from "react-router-dom";
-import ItemDetailContainer from "./components/Items/Item/ItemDetail";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import ItemsContainer from "./components/Items/ItemsContainer";
 import "./App.css";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route index path="/" element={<ItemListContainer />}></Route>
           <Route path="/category/:name" element={<ItemListContainer />}></Route>
-          <Route path="/item/:id" element={<ItemDetailContainer />}></Route>
+          <Route path="/item/:id" element={<ItemsContainer />}></Route>
         </Routes>
         <NavFooter />
       </BrowserRouter>
